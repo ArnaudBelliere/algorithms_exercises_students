a728ddf4a18c9219a7b2b211e37dec0ca600319e
Bibinado <arnaud.belliere@student.uclouvain.be>
2024-10-03 10:30:20 +0200

    cardsorter ok

--- src/main/java/sorting/CardSorter.java
+++ src/main/java/sorting/CardSorter.java
@@ -23,12 +23,19 @@ public class CardSorter {
     public static void sort(LinkedListImpl list) {
         // Here is a small loop with an invariant that you should try to respect
         // although, it's not mandatory, try to respect it, it will help you ;-)
-
         for (int iter = 0; iter < list.getSize() - 1; iter++) {
             //invariant: the 'iter' biggest elements are at the end of the list and are sorted.
             //example, at iteration iter=3, the three lasts elements are the three biggest elements in the list, and
             //they are in the increasing order.
-
+            for ( int i = 0 ; i < list.getSize() - 1 - iter; i ++){
+                if (list.getFirst() > list.getSecond()){
+                    list.swap();
+                }
+                list.pop();
+            }
+            for (int i = 0; i < iter+1; i++){
+                list.pop();
+            }
             // TODO
 
         }
