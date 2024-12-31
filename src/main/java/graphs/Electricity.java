a320915c72b7f0959911963c22d118751b0396b1
Bibinado <arnaud.belliere@student.uclouvain.be>
2024-12-31 01:13:38 +0100

    does not connect the set in the graph

--- src/main/java/graphs/Electricity.java
+++ src/main/java/graphs/Electricity.java
@@ -33,7 +33,71 @@ public class Electricity {
         // the method to find the min spanning tree is too first sort the edges by their cost ( ascending ), then take all edges in order.
         // when the edge taken forms a cycle ( i.e. the two nodes already are in the previously selected edges ), we skip it.
         // comp should be O(n^2) ( while e : edges { if e in selectedEdges { .. } }
-        return -1;
+        int cost = 0;
+        ArrayList<int[]> sortedList = new ArrayList<>(Arrays.asList(edges));
+        sortedList.sort(Comparator.comparingInt(e -> e[2]));
+        ArrayList<Integer> visited = new ArrayList<>();
+
+        for (int i = 0 ; i < sortedList.size() ; i ++){
+            int curr = 2;
+            int e1 = sortedList.get(i)[0];
+            int e2 = sortedList.get(i)[1];
+            if ( !visited.contains(e1)){
+                visited.add(e1);
+                curr --;
+            }
+            if ( !visited.contains(e2)){
+                visited.add(e2);
+                curr --;
+            }
+            if ( curr < 2){
+                cost += sortedList.get(i)[2];
+                //System.out.println(" e1 : " + e1 + ", e2 : " + e2 + ", cost : " + sortedList.get(i)[2]);
+            }
+            if ( visited.size() >= n ){
+                break;
+            }
+        }
+
+        return cost;
     }
 
+            /*
+        System.out.println("Before Sort : ");
+        for (int[] edge : sortedList) {
+            System.out.println(Arrays.toString(edge));
+        }
+        // already sorted ?
+        /*sortedList.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] e1, int[] e2) {
+                return Integer.compare(e1[2], e2[2]);
+            }
+        });
+        System.out.println("After Sort : ");
+        for (int[] edge : sortedList) {
+            System.out.println(Arrays.toString(edge));
+        }*/
+
+    /*
+            for (int i = 0 ; i < edges.length ; i ++){
+            int curr = 2;
+            int e1 = edges[i][0];
+            int e2 = edges[i][1];
+            if ( !visited.contains(e1)){
+                visited.add(e1);
+                curr --;
+            }
+            if ( !visited.contains(e2)){
+                visited.add(e2);
+                curr --;
+            }
+            if ( curr < 2){
+                cost += edges[i][2];
+            }
+            if ( visited.size() >= n ){
+                break;
+            }
+        }
+        */
 }
